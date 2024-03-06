68688c9912a23240ce9c140573acbf9440b8f364
Xavier Lau <xavier@inv.cafe>
2024-03-06 10:24:30 +0800

    Release V2 (#1)
    * Release V2
    
    * Refactor
    
    * Fix

---
+++ .github/wasm.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import os, subprocess, sys
+
+tag = sys.argv[1]
+runtime = sys.argv[2]
+features = sys.argv[3]
+features_ = features.replace(",", "-")
+cmd = [
+    "rtor",
+    "-g",
+    "https://github.com/darwinia-network/darwinia",
+    "-t",
+    tag,
+    "-m",
+    f"runtime/{runtime}/Cargo.toml",
+    "-r",
+    runtime,
+    "-f",
+    features,
+    "-o",
+    f"wasm/{features_}",
+]
+
+print(" ".join(cmd))
+subprocess.run(cmd).check_returncode()
+
+dir = f"wasm/{features_}"
+os.makedirs(dir, exist_ok=True)
+
+with open(f"{dir}/CHANGELOG", "a+") as f:
+    f.write(f'Generated {tag} at {datetime.now().strftime("%m/%d/%Y %H:%M:%S")}\n')
